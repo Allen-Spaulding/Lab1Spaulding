417277d5b4346b33d80b5942789dd2e2c02b5eaa
Allen-Spaulding <aspauld3@rockets.utoledo.edu>
2024-04-05 12:23:17 -0700

    first commit

---
+++ Laboratory.cpp
@@ -0,0 +1,25 @@
+/*
+ * Program:  <First Laboratory>
+ * Author:   <Allen Spaulding>
+ * Date:     <01/26/2024>
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle Area = " << area << "\n";
+
+	cout << "Who's on First? \n";
+
+	return 0;
+}
